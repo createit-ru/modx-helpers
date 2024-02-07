9cd3d3b739963b55af72c286f51d74d87d9a4ce1
Alexey <info@createit.ru>
2024-02-07 14:01:44 +0300

    Update seoTitle.php

--- seo/seoTitle.php
+++ seo/seoTitle.php
@@ -1,19 +1,16 @@
 <?php
 
-$result = '';
-
 $result = $modx->resource->getTVValue("seoTitle");
 if(empty($result)){
     $result = $modx->resource->get("longtitle");
-}
-if(empty($result)){
-    $result = $modx->resource->get("pagetitle");
+    if(empty($result)){
+        $result = $modx->resource->get("pagetitle");
+    }
 }
 
 // для всех страниц, кроме главной, добавляем имя сайта к заголовку
-if($modx->config["site_start"] != $modx->resource->get("id")){
-  $sitename = $modx->config["site_name"];
-  $result = $result." - ".$sitename;
+if($modx->getOption("site_start") != $modx->resource->get("id")){
+  $result = $result . " - " . $modx->getOption("site_start");;
 }
 
 return $result;
